afb0d95235c68f49902de5ce799c537da78bea37
ivanu <ivan.udovychenko.25@kse.org.ua>
2026-04-08 12:16:09 +0300

    Створив код для виведення графіка рівня депресії та неспокою в 5-ти країнах з найвищими рівнями депресії та неспокою.

--- pandas_plt_ps.py
+++ pandas_plt_ps.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import kagglehub
+import matplotlib.pyplot as plt
 
 # python.exe -m pip install --upgrade pip
 
@@ -13,9 +14,23 @@ df = pd.read_csv("datasets/Global_Mental_Health_Crisis_Index_2026.csv")
 # df["gdp_geq_40k_low_help"] = df["population_millions"] * (df["treatment_gap_pct"]/100)
 # print(df["lack_of_care_mln"])
 
-gdp_geq_40k = df[df["gdp_per_capita_usd"] >= 40000]
-gdp_geq_40k_low_help = gdp_geq_40k[gdp_geq_40k["mh_system_score"] <5]
-gdp_geq_40k_low_help["lack_of_care_mln"] = (gdp_geq_40k_low_help["population_millions"] *
-                                            (gdp_geq_40k["treatment_gap_pct"]/100))
+# gdp_geq_40k = df[df["gdp_per_capita_usd"] >= 40000]
+# gdp_geq_40k_low_help = gdp_geq_40k[gdp_geq_40k["mh_system_score"] <5]
+# gdp_geq_40k_low_help["lack_of_care_mln"] = (gdp_geq_40k_low_help["population_millions"] *
+#                                             (gdp_geq_40k["treatment_gap_pct"]/100))
+#
+# print(gdp_geq_40k_low_help[["country","mh_system_score","gdp_per_capita_usd","lack_of_care_mln"]])
 
-print(gdp_geq_40k_low_help[["country","mh_system_score","gdp_per_capita_usd","lack_of_care_mln"]])
+top_5 = df.nlargest(5, "depression_pct")
+print(top_5)
+
+plt.figure(figsize = (8,4))
+plt.plot(top_5["country"],top_5["depression_pct"],
+         marker='o',label="anxiety" )
+plt.plot(top_5["country"],top_5["anxiety_pct"],
+         marker='o', label="depression")
+plt.title("Anxiety vs Depression (%)")
+plt.xlabel("Country")
+plt.ylabel("Percents")
+plt.legend()
+plt.show()
